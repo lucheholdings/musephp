7061375660688cd1c581631601362acf16cf18ae
Yoshi Aoki <yoshi@44services.jp>
2014-10-08 17:26:17 +0900

    fix factory methods

--- clio/src/Clio/Adapter/SymfonyBundles/FrameworkBundle/Kvs/Factory/CachedMapFactory.php
+++ clio/src/Clio/Adapter/SymfonyBundles/FrameworkBundle/Kvs/Factory/CachedMapFactory.php
@@ -18,43 +18,81 @@ use Clio\Bridge\DoctrineCache\Container\CachedMap;
 
 class KvsFactoryMap extends FactoryMap implements ContainerAwareInterface 
 {
+	/**
+	 * container 
+	 * 
+	 * @var mixed
+	 * @access private
+	 */
 	private $container;
 
+	/**
+	 * cacheFactory 
+	 * 
+	 * @var mixed
+	 * @access private
+	 */
 	private $cacheFactory;
 
-	public function createArgs($alias, $cache = null, array $args = array())
+	/**
+	 * __construct 
+	 * 
+	 * @param ContainerInterface $container 
+	 * @param mixed $cacheFactory 
+	 * @access public
+	 * @return void
+	 */
+	public function __construct(ContainerInterface $container = null, $cacheFactory = null)
 	{
-		// 
-		$map = parent::createArgs($alias, $args);
+		$this->container = $container;
+		$this->cacheFactory = $cacheFactory;
+	}
+
+	/**
+	 * doCreate 
+	 * 
+	 * @param array $args 
+	 * @access protected
+	 * @return void
+	 */
+	protected function doCreate(array $args)
+	{
+		$key = array_shift($args);
+		$cache = array_shift($args);
+
+		return $this->createKvsByKeyArgs($key, $cache, $args);
+	}
+
+	/**
+	 * createKvsByKeyArgs 
+	 * 
+	 * @param mixed $key 
+	 * @param mixed $cache 
+	 * @param array $args 
+	 * @access public
+	 * @return void
+	 */
+	public function createKvsByKeyArgs($key, $cache = null, array $args = array())
+	{
+		//  
+		$map = parent::createByKeyArgs($key, $args);
 
 		if($cache) {
-			$map = $this->createCachedMap($map, $cacheType);
+			$map = $this->doCreateCachedMap($map, $cacheType);
 		}
 
 		return $map; 
 	}
 
 	/**
-	 * create 
-	 *   
-	 * @access public
-	 * @param  string Alias of Factory 
-	 * @param  string "array" or Alias of Cache in DI
+	 * doCreateCachedMap 
+	 * 
+	 * @param mixed $map 
+	 * @param mixed $cache 
+	 * @access protected
 	 * @return void
 	 */
-	public function create()
-	{
-		$args = func_get_args();
-
-		$alias = array_shift($args);
-		$cache = array_shift($args);
-
-		return $this->createArgs($alias, $cache, $args);
-	}
-
-
-
-	protected function createCachedMap($map, $cache)
+	protected function doCreateCachedMap($map, $cache)
 	{
 		// Create DoctrineCache if needed
 		if(is_string($cache) && $this->getContainer()->has($cache)) {
@@ -69,22 +107,48 @@ class KvsFactoryMap extends FactoryMap implements ContainerAwareInterface
 		return new CachedMap($map, $cache);
 	}
     
+    /**
+     * getContainer 
+     * 
+     * @access public
+     * @return void
+     */
     public function getContainer()
     {
         return $this->container;
     }
     
+    /**
+     * setContainer 
+     * 
+     * @param ContainerInterface $container 
+     * @access public
+     * @return void
+     */
     public function setContainer(ContainerInterface $container)
     {
         $this->container = $container;
         return $this;
     }
     
+    /**
+     * getCacheFactory 
+     * 
+     * @access public
+     * @return void
+     */
     public function getCacheFactory()
     {
         return $this->cacheFactory;
     }
     
+    /**
+     * setCacheFactory 
+     * 
+     * @param mixed $cacheFactory 
+     * @access public
+     * @return void
+     */
     public function setCacheFactory($cacheFactory)
     {
         $this->cacheFactory = $cacheFactory;
