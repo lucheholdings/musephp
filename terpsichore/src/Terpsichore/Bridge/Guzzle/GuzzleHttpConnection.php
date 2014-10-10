dd223eded05c2360b87c4ba577acaa1ce9881dca
Yoshi Aoki <yoshi@44services.jp>
2014-10-10 11:52:15 +0900

    fix namespace for Client

--- terpsichore/src/Terpsichore/Bridge/Guzzle/GuzzleHttpConnection.php
+++ terpsichore/src/Terpsichore/Bridge/Guzzle/GuzzleHttpConnection.php
@@ -1,8 +1,8 @@
 <?php
 namespace Terpsichore\Bridge\Guzzle;
 
-use Terpsichore\Core\Connection\HttpConnection;
-use Terpsichore\Core\Request;
+use Terpsichore\Client\Connection\HttpConnection;
+use Terpsichore\Client\Request;
 use GuzzleHttp\Client as GuzzleClient;
 
 /**
