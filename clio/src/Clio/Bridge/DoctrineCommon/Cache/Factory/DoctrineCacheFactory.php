42bbb773fb03bcefc72f9df309f3af69d50e7439
Yoshi Aoki <yoshi@44services.jp>
2014-11-24 03:05:44 +0900

    add createCache and createCacheProvider method

--- clio/src/Clio/Bridge/DoctrineCommon/Cache/Factory/DoctrineCacheFactory.php
+++ clio/src/Clio/Bridge/DoctrineCommon/Cache/Factory/DoctrineCacheFactory.php
@@ -6,10 +6,14 @@ use Clio\Bridge\DoctrineCommon\Cache\DoctrineCache;
 
 class DoctrineCacheFactory extends AbstractDoctrineCacheFactory 
 {
+	public function createCache($type, array $args = array())
+	{
+		return $this->createByKeyArgs($type, $args);
+	}
+
 	protected function getCacheClass()
 	{
 		return new \ReflectionClass('Clio\Bridge\DoctrineCommon\Cache\DoctrineCache');
 	}
-
 }
 
--- clio/src/Clio/Bridge/DoctrineCommon/Cache/Factory/DoctrineCacheProviderFactory.php
+++ clio/src/Clio/Bridge/DoctrineCommon/Cache/Factory/DoctrineCacheProviderFactory.php
@@ -1,10 +1,13 @@
 <?php
 namespace Clio\Bridge\DoctrineCommon\Cache\Factory;
 
-
-
 class DoctrineCacheProviderFactory extends AbstractDoctrineCacheFactory 
 {
+	public function createCacheProvider($type, array $args = array())
+	{
+		return $this->createByKeyArgs($type, $args);
+	}
+
 	protected function getCacheClass()
 	{
 		return new \ReflectionClass('Clio\Bridge\DoctrineCommon\Cache\DoctrineCacheProvider');
