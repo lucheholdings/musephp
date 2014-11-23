ef9e704cbffc1b4ff725148a715d1c2cf7c509f0
Yoshi Aoki <yoshi@44services.jp>
2014-11-23 20:58:07 +0900

    add normalizer section on config

--- clio/src/Clio/Adapter/SymfonyBundles/ComponentBundle/DependencyInjection/Configuration.php
+++ clio/src/Clio/Adapter/SymfonyBundles/ComponentBundle/DependencyInjection/Configuration.php
@@ -24,6 +24,53 @@ class Configuration implements ConfigurationInterface
         // configure your bundle. See the documentation linked above for
         // more information on that topic.
 
+		$rootNode
+			->addDefaultsIfNotSet()
+			->children()
+				->append($this->buildNormalizerSection())
+			->end()
+		;
+
         return $treeBuilder;
     }
+
+	protected function buildNormalizerSection()
+	{
+		$treeBuilder = new TreeBuilder();
+		$node = $treeBuilder->rootNode('normalizer');
+
+		$node
+			->canBeDisabled()
+			->addDefaultsIfNotSet()
+			->children()
+				->arrayNode('strategies')
+			->end()
+		;
+
+		$strategyNodes = $node->getChild('strategies')->children();
+		
+
+		foreach(array() as $name) {
+			$this->addNormalizerStrategySection($strategyNodes, $name);
+		}
+
+		return $node;
+	}
+
+	protected function addNormalizerStrategySection($parentNode)
+	{
+		$parentNode
+			->arrayNode($name)
+				->canBeDisabled()
+				->beforeNormalization()
+					->ifString()
+					->then(function($v){
+						return array('enabled' => true, 'id' => $v);
+					})
+				->end()
+				->children()
+					->scalarNode('id')->defaultValue('clio_component.normalizer.default_strategy.' . $name)
+				->end()
+		;
+	}
 }
