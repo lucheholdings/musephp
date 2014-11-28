420815923b1412b112bf74c79fd7b5022664573d
Yoshi Aoki <yoshi@44services.jp>
2014-11-28 23:00:30 +0900

    add Refernece CompilerPass into Bundle registration

--- clio/src/Clio/Adapter/SymfonyBundles/ComponentBundle/ClioComponentBundle.php
+++ clio/src/Clio/Adapter/SymfonyBundles/ComponentBundle/ClioComponentBundle.php
@@ -3,7 +3,53 @@
 namespace Clio\Adapter\SymfonyBundles\ComponentBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Clio\Adapter\SymfonyBundles\ComponentBundle\DependencyInjection\Compiler;
 
 class ClioComponentBundle extends Bundle
 {
+	public function build(ContainerBuilder $container)
+	{
+		parent::build($container);
+
+		$container->addCompilerPass(new Compiler\ReferenceCompilerPass());
+	}
+
+    public function registerCommands(Application $application)
+	{
+		parent::registerCommands($application);
+
+		$this->registerHelpers($application);
+	}
+
+	protected function registerHelpers(Application $application)
+	{
+        if (!is_dir($dir = $this->getPath().'/Console/Helper')) {
+            return;
+        }
+
+        $finder = new Finder();
+        $finder->files()->name('*Helper.php')->in($dir);
+
+        $prefix = $this->getNamespace().'\\Console\\Helper';
+        foreach ($finder as $file) {
+            $ns = $prefix;
+            if ($relativePath = $file->getRelativePath()) {
+                $ns .= '\\'.strtr($relativePath, '/', '\\');
+            }
+            $class = $ns.'\\'.$file->getBasename('.php');
+            //if ($this->container) {
+            //    $alias = 'console.helper.'.strtolower(str_replace('\\', '_', $class));
+            //    if ($this->container->has($alias)) {
+            //        continue;
+            //    }
+            //}
+            $r = new \ReflectionClass($class);
+            if ($r->implementsInterface('Symfony\\Component\\Console\\Helper\\HelperInterface') && !$r->isAbstract() && (!$r->getConstructor() || !$r->getConstructor()->getNumberOfRequiredParameters())) {
+                $application->getHelperSet()->set($r->newInstance());
+            }
+        }
+	}
 }
