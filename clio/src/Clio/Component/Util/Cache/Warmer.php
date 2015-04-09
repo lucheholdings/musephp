09340efa64d428921218f01dabf260d82955f63f
Yoshi Aoki <yoshi@44services.jp>
2015-04-09 16:22:56 +0900

    update cache component

---
+++ clio/src/Clio/Component/Util/Cache/Warmer.php
@@ -0,0 +1,22 @@
+<?php
+namespace Clio\Component\Util\Cache;
+
+/**
+ * Warmer 
+ *   Interface of CacheWarmer.
+ * @package { PACKAGE }
+ * @copyright Copyrights (c) 1o1.co.jp, All Rights Reserved.
+ * @author Yoshi<yoshi@1o1.co.jp> 
+ * @license { LICENSE }
+ */
+interface Warmer
+{
+    /**
+     * warmup 
+     * 
+     * @access public
+     * @return void
+     */
+    function warmup();
+}
+
