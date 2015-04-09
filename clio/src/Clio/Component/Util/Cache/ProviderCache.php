db1cfb042759025894627c016bdec5a44bb64c19
Yoshi Aoki <yoshi@44services.jp>
2015-04-09 16:22:56 +0900

    update cache component

--- clio/src/Clio/Component/Util/Cache/ProviderCache.php
+++ clio/src/Clio/Component/Util/Cache/ProviderCache.php
@@ -1,6 +1,16 @@
 <?php
 namespace Clio\Component\Util\Cache;
 
+/**
+ * ProviderCache 
+ *   ProviderCache is a Cache which acts as an entry of CacheProvider. 
+ *   
+ * @uses Cache
+ * @package { PACKAGE }
+ * @copyright Copyrights (c) 1o1.co.jp, All Rights Reserved.
+ * @author Yoshi<yoshi@1o1.co.jp> 
+ * @license { LICENSE }
+ */
 class ProviderCache implements Cache
 {
 	/**
