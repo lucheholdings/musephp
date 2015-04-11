ac06eced415ff2c8c7df911b8df8432e44704129
Yoshi Aoki <yoshi@44services.jp>
2015-04-11 19:49:19 +0900

    replace RegistryMap with MapRegistry which replace the dependencies

---
+++ clio/src/Clio/Component/Pattern/Registry/MapRegistry.php
@@ -0,0 +1,66 @@
+<?php
+namespace Clio\Component\Pattern\Registry;
+
+use Clio\Component\Util\Container\Map\StorageMap;
+
+/**
+ * MapRegistry 
+ * 
+ * @uses StorageMap 
+ * @uses Registry
+ * @package { PACKAGE }
+ * @copyright Copyrights (c) 1o1.co.jp, All Rights Reserved.
+ * @author Yoshi<yoshi@1o1.co.jp> 
+ * @license { LICENSE }
+ */
+class MapRegistry implements Registry 
+{
+    private $registered;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function has($key)
+    {
+        return isset($this->registered[$key]);
+    }
+    /**
+     * {@inheritdoc}
+     */
+	public function get($key)
+    {
+        return $this->registered[$key];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+	public function set($key, $entry)
+    {
+        $this->registered[$key] = $entry;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+	public function remove($key)
+    {
+        unset($this->registered[$key]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+	public function clear()
+    {
+        $this->registered = array();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+	public function count()
+    {
+        return count($this->registered);
+    }
+}
