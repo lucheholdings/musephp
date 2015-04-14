01bf9c6c45b84b1abe86daf47808fccc3922489e
Yoshi Aoki <yoshi@44services.jp>
2015-04-14 14:26:01 +0900

    update testcase

--- clio/src/Clio/Component/Tool/Normalizer/Tests/Strategy/ScalarStrategyTest.php
+++ clio/src/Clio/Component/Tool/Normalizer/Tests/Strategy/ScalarStrategyTest.php
@@ -1,64 +1,20 @@
 <?php
 namespace Clio\Component\Tool\Normalizer\Tests\Strategy;
 
+use Clio\Component\Tool\Normalizer\Tests\StrategyTestCase;
 use Clio\Component\Tool\Normalizer\Strategy\ScalarStrategy;
-use Clio\Component\Tool\Normalizer\Type\NormalizerType;
-use Clio\Component\Util\Type\Actual as ActualTypes;
-use Clio\Component\Util\Type as Types;
 
 class ScalarStrategyTest extends StrategyTestCase 
 {
-	private $testData;
-
-	private $testTypeName;
-
-	public function testNormalize()
-	{
-		return;
-	}
-
-	public function testNormalizeString()
-	{
-		$this->testData = 'Foo';
-		$this->testTypeName = 'string';
-		parent::testNormalize();
-	}
-
-	public function testNormalizeInt()
-	{
-		$this->testData = 1;
-		$this->testTypeName = Types\PrimitiveTypes::TYPE_INT;
-		parent::testNormalize();
-	}
-
-    public function testDenormalize()
+    public function testBasic()
     {
-        return ;
+        $this->strategy = new ScalarStrategy();
+        $this->type     = $this->getContext()->getTypeResolver()->resolve('string');
+        $this->normalizedData   = 'foo';
+        $this->denormalizedData = 'foo';
+
+        $this->doTestNormalize();
+        $this->doTestDenormalize();
     }
-
-	protected function createStrategy()
-	{
-		return new ScalarStrategy();
-	}
-
-	protected function getTestData()
-	{
-		return $this->testData;
-	}
-
-	protected function getResultData()
-	{
-		return $this->testData;
-	}
-
-	protected function createType($data)
-	{
-		return new NormalizerType(new ActualTypes\ScalarType(gettype($data)));
-	}
-
-	protected function getTestTypeName()
-	{
-		return $this->testTypeName;
-	}
 }
 
