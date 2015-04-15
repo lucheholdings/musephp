29950c5a8883b57d59a36f1777f937ff8ad93e02
Yoshi Aoki <yoshi@44services.jp>
2015-04-15 16:06:45 +0900

    update Extra Normalizer Component

---
+++ clio/src/Clio/Extra/Normalizer/Strategy/Factory.php
@@ -0,0 +1,45 @@
+<?php
+namespace Clio\Extra\Normalizer\Strategy;
+
+use Clio\Component\Tool\Normalizer\Strategy;
+use Clio\Component\Util\Accessor\Registry as RegistryInterface;
+
+/**
+ * Factory 
+ * 
+ * @abstract
+ * @package { PACKAGE }
+ * @copyright Copyrights (c) 1o1.co.jp, All Rights Reserved.
+ * @author Yoshi<yoshi@1o1.co.jp> 
+ * @license { LICENSE }
+ */
+abstract class Factory extends Strategy\Factory
+{
+    /**
+     * createDefaultSet 
+     * 
+     * @param RegistryInterface $accessorRegsitry 
+     * @static
+     * @access public
+     * @return void
+     */
+    static public function createDefaultSet(RegistryInterface $accessorRegistry = null) 
+    {
+        if($accessorRegistry) {
+            return new Strategy\PriorityCollection(array(
+                    // 
+		    	    new Strategy\MixedStrategy(),
+		    	    new Strategy\ReferenceStrategy(),
+                    // 
+                    new Strategy\NullStrategy(),
+                    // Custom Strategy
+
+                    // Accessor Strategy
+		    	    new AccessorStrategy($accessorRegistry),
+                ));
+        } else {
+            return parent::createDefaultSet();
+        }
+    }
+}
+
