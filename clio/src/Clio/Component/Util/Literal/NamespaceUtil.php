bc92160f9500f5abcf25cc013da2e3af1eb797d1
Yoshi Aoki <yoshi@44services.jp>
2015-04-20 09:52:45 +0900

    add Literal Component which will replace Grammer and Psr

---
+++ clio/src/Clio/Component/Util/Literal/NamespaceUtil.php
@@ -0,0 +1,54 @@
+<?php
+namespace Clio\Component\Util\Literal;
+
+/**
+ * NamespaceUtil 
+ *   Convert Namespace "\\" to other.
+ * 
+ * @package { PACKAGE }
+ * @copyright Copyrights (c) 1o1.co.jp, All Rights Reserved.
+ * @author Yoshi<yoshi@1o1.co.jp> 
+ * @license { LICENSE }
+ */
+class NamespaceUtil 
+{
+    /**
+     * dirPath 
+     * 
+     * @param mixed $namespace 
+     * @static
+     * @access public
+     * @return void
+     */
+    static public function dirPath($namespace)
+    {
+        return str_replace('\\', DIRECTORY_SEPARATOR, $namespace);
+    }
+
+    /**
+     * slashPath 
+     * 
+     * @param mixed $namespace 
+     * @static
+     * @access public
+     * @return void
+     */
+    static public function slashPath($namespace)
+    {
+        return str_replace('\\', '/', $namespace);
+    }
+
+    /**
+     * dotPath 
+     * 
+     * @param mixed $namespace 
+     * @static
+     * @access public
+     * @return void
+     */
+    static public function dotPath($namespace)
+    {
+        return str_replace('\\', '.', $namespace);
+    }
+}
+
