3ccf9cc33dd1b96784e68bb5b329c60f07626cd6
Yoshi Aoki <yoshi@44services.jp>
2015-04-23 00:25:54 +0900

    add Uuid Generators

---
+++ clio/src/Clio/Adapter/DoctrineExtensions/Id/AbstractStrategyGenerator.php
@@ -0,0 +1,43 @@
+<?php
+namespace Clio\Adapter\DoctrineExtensions\Id;
+
+use Doctrine\ORM\Id\AbstractIdGenerator;
+use Doctrine\ORM\EntityManager;
+
+use Clio\Component\Util\Id\Generator\Strategy;
+
+/**
+ * AbstractStrategyGenerator 
+ * 
+ * @uses AbstractIdGenerator
+ * @abstract
+ * @package { PACKAGE }
+ * @copyright Copyrights (c) 1o1.co.jp, All Rights Reserved.
+ * @author Yoshi<yoshi@1o1.co.jp> 
+ * @license { LICENSE }
+ */
+abstract class AbstractStrategyGenerator extends AbstractIdGenerator 
+{
+	/**
+	 * generate 
+	 * 
+	 * @param EntityManager $em 
+	 * @param mixed $entity 
+	 * @access public
+	 * @return void
+	 */
+	public function generate(EntityManager $em, $entity)
+	{
+        return $this->getStrategy()->generate();
+	}
+
+    /**
+     * getStrategy 
+     * 
+     * @abstract
+     * @access protected
+     * @return void
+     */
+    abstract protected function getStrategy();
+}
+
