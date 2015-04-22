138bff6352f2af18b1152f1e1983509259c1ec1a
Yoshi Aoki <yoshi@44services.jp>
2015-04-23 00:25:54 +0900

    add Uuid Generators

---
+++ clio/src/Clio/Adapter/DoctrineExtensions/Id/Uuid1Generator.php
@@ -0,0 +1,32 @@
+<?php
+namespace Clio\Adapter\DoctrineExtensions\Id;
+
+use Clio\Component\Util\Id\Generator\Strategy\UuidGenerateStrategy;
+
+/**
+ * Uuid1Generator 
+ *    
+ * @uses AbstractIdGenerator
+ * @package { PACKAGE }
+ * @copyright { COPYRIGHT } (c) { COMPANY }
+ * @author Yoshi Aoki <yoshi@44services.jp> 
+ * @license { LICENSE }
+ */
+class Uuid1Generator extends AbstractStrategyGenerator
+{
+    /**
+     * getStrategy 
+     * 
+     * @access protected
+     * @return void
+     */
+    protected function getStrategy()
+    {
+        if(!$this->strategy) {
+            $this->strategy = new UuidGenerateStrategy(1);
+        }
+
+        return $this->strategy;
+    }
+}
+
