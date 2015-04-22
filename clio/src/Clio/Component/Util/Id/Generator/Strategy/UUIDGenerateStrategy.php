17fd9997e670eb1f0be8dc26d93ae67a963cc80a
Yoshi Aoki <yoshi@44services.jp>
2015-04-22 22:50:16 +0900

    add commnets and fix the class lettercase

--- clio/src/Clio/Component/Util/Id/Generator/Strategy/UUIDGenerateStrategy.php
+++ clio/src/Clio/Component/Util/Id/Generator/Strategy/UUIDGenerateStrategy.php
@@ -3,12 +3,34 @@ namespace Clio\Component\Util\Id\Generator\Strategy;
 
 use Clio\Component\Util\Id\Generator\Strategy;
 
-class UUIDGenerateStrategy implements Strategy 
+/**
+ * UuidGenerateStrategy 
+ * 
+ * @uses Strategy
+ * @package { PACKAGE }
+ * @copyright Copyrights (c) 1o1.co.jp, All Rights Reserved.
+ * @author Yoshi<yoshi@1o1.co.jp> 
+ * @license { LICENSE }
+ */
+class UuidGenerateStrategy implements Strategy 
 {
     const DEFAULT_VERSION  = 4;
 
+    /**
+     * version 
+     * 
+     * @var mixed
+     * @access private
+     */
     private $version;
 
+    /**
+     * __construct 
+     * 
+     * @param mixed $version 
+     * @access public
+     * @return void
+     */
     public function __construct($version = self::DEFAULT_VERSION)
     {
 		// Create UUID
@@ -30,6 +52,12 @@ class UUIDGenerateStrategy implements Strategy
         }
     }
 
+    /**
+     * generate 
+     * 
+     * @access public
+     * @return void
+     */
     public function generate()
     {
         return uuid_create($this->version);
